e0a87d2519175a9c7c0a12b1f7af051b130231b2
Jonathan Lalande <lalandej.gg@gmail.com>
2024-02-03 12:52:02 -0500

    gitignore + SDL window

--- main.c
+++ main.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 
+#include <SDL2/SDL.h>
+
+#define WINDOW_W 1024
+#define WINDOW_H 768
+
 int main(int argc, char *argv[]) {
-    printf("hello!");
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        printf("Couldn't initialize SDL: %s\n", SDL_GetError());
+        exit(1);
+    }
+
+    struct SDL_Window *window = SDL_CreateWindow("Tetris", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_W, WINDOW_H, 0);
+    if (window == NULL) {
+        printf("Couldn't initialize SDL window: %s\n", SDL_GetError());
+        exit(1);
+    }
+
+    struct SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL) {
+        printf("Couldn't initialize SDL renderer: %s\n", SDL_GetError());
+        exit(1);
+    }
 }
